805527310fc3ab850c4a1461ec803683edfa915f
Jack Ainochi <kthrkdals@gmail.com>
2020-02-15 17:33:27 +0900

    20200215 change project name & book ch2 obj, var, dynamic}

---
+++ Book/Book_01/RoadBook.CsharpBasic.Chapter01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadBook.CsharpBasic.Chapter01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Examples 폴더 안에서 Hello 클래스를 불러들여 Run 기능을 수행하라.
+            Examples.Hello hello = new Examples.Hello();
+            hello.Run();
+        }
+    }
+}
---
+++ Book/Book_01/RoadBook.CsharpBasic.Chapter02.Examples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadBook.CsharpBasic.Chapter02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Examples.Ex001 ex001 = new Examples.Ex001();
+            ex001.Run();
+            Console.WriteLine("\n");
+            Examples.Ex002 ex002 = new Examples.Ex002();
+            ex002.Run();
+            Console.WriteLine("\n");
+            Examples.Ex003 ex003 = new Examples.Ex003();
+            ex003.Run();
+            Console.WriteLine("\n");
+            Examples.Ex004 ex004 = new Examples.Ex004();
+            ex004.Run(); 
+            Console.WriteLine("\n");
+            Examples.Ex005 ex005 = new Examples.Ex005();
+            ex005.Run();
+        }
+    }
+}
