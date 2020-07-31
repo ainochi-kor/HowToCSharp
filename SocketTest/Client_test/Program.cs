c5fa72d97531fcc2fd24e7721b611f382f653529
ainochi-kor <kthrkdals@gmail.com>
2020-07-31 15:27:26 +0900

    add

---
+++ SocketTest/Client_test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client_test
+{
+    class Program
+    {
+        Socket trensferSocket;
+        static void Main(string[] args)
+        {
+            Socket trensferSocket = new Socket(
+                AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //trensferSocket.Connect(new IPEndPoint(IPAddress.Parse("10.177.55.73"), 1234));
+            IPEndPoint companyEndPoint = new IPEndPoint(IPAddress.Parse("10.177.55.39"), 8080);
+            try
+            {
+                trensferSocket.Connect(companyEndPoint);
+            }
+            catch
+            {
+                Console.WriteLine("Unable to connect to remote and point! \r\n");
+                Main(args);
+            }
+
+            Console.Write("Enter Text: ");
+            string text = Console.ReadLine();
+            byte[] data = Encoding.UTF8.GetBytes(text);
+
+            trensferSocket.Send(data);
+            Console.Write("Data Sent! \r\n");
+            Console.Write("Press any key To continue... ");
+            Console.Read();
+            trensferSocket.Close();
+        }
+    }
+}
---
+++ SocketTest/WTP_ManyText_ClientTest/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace WTP_ManyText_ClientTest
+{
+    public partial class Form1 : Form
+    {
+        Socket socket;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (socket.Connected)
+                socket.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(new IPEndPoint(IPAddress.Parse("10.177.55.73"), 1234));
+                MessageBox.Show("연결되었습니다.");
+            }
+            catch
+            {
+                MessageBox.Show("Unable to connect!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(textBox1.Text);
+
+            socket.Send(BitConverter.GetBytes(data.Length), 0, 4, 0);
+            socket.Send(data);
+        }
+    }
+}
